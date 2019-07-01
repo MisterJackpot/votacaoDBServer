ad8523152dbd66f4bdeeeba5e33b9528c3dcc4ff
Gabriel Loff <gabriel.loff@acad.pucrs.br>
2019-07-01 18:12:09 -0300

    Passagem entre telas

--- src/main/java/sample/Controller/Controller.java
+++ src/main/java/sample/Controller/Controller.java
@@ -1,5 +1,40 @@
 package sample.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Controller {
+
+    @FXML
+    ChoiceBox users;
+
+    @FXML
+    private void initialize()
+    {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Gabriel");
+        list.add("Joao");
+
+
+        users.setItems(FXCollections.observableArrayList(list));
+    }
+
+    @FXML
+    public void entrar(){
+        try {
+            Parent pagina = (Parent) FXMLLoader.load(getClass().getResource("/principal.fxml"));
+            Scene scene = users.getScene();
+            scene.setRoot(pagina);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
